bcae349918df83b66a6685a3849fc83e0132c678
mbutsk <maratik1036@gmail.com>
2025-03-28 16:42:31 +0300

    photo and commands support to patch_me function in Bot class

--- aiomax/api.py
+++ aiomax/api.py
@@ -1,4 +1,5 @@
 import requests
+from . import classes
 
 class Bot:
     def __init__(self, access_token: str):
@@ -25,12 +26,29 @@ class Bot:
         '''
         info = await self.get(f"https://botapi.max.ru/me")
         return info.json()
-    
-    async def patch_me(self, **kwargs):
+
+    async def patch_me(self, name: str = None, description: str = None, commands: list[classes.BotCommand] = None, photo: classes.PhotoAttachmentRequestPayload = None):
         '''
         Allows you to change information about the current bot. Fill in only those fields that need to be updated. All others will remain unchanged.
+        
         :param name: Bot display name
         :param description: Bot description
+        :param commands: Commands supported by the bot. To remove all commands, pass an empty list.
+        :param photo: Request to set up a bot photo
         '''
-        response = self.session.patch(f"https://botapi.max.ru/me", json=kwargs)
+        if commands:
+            commands = [i.as_dict() for i in commands]   
+        if photo:
+            photo = photo.as_dict()
+        
+        vars = {
+            "name": name,
+            "description": description,
+            "commands": commands,
+            "photo": photo
+                }
+
+        payload = {k: v for k, v in vars.items() if v}
+
+        response = self.session.patch(f"https://botapi.max.ru/me", json=payload)
         return response.json()
---
+++ aiomax/classes.py
@@ -0,0 +1,14 @@
+class BotCommand:
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+    
+    def as_dict(self):
+        return {"name": self.name, "description": self.description}
+
+class PhotoAttachmentRequestPayload:
+    def __init__(self, url: str | None):
+        self.url = url
+    
+    def as_dict(self):
+        return {"url": self.url}
