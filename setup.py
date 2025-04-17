c72a978663dccddb893ea6cc8a8eb5b2e1c9b51c
moontr3 <rodionkovalkovskii@gmail.com>
2025-04-17 17:48:24 +0300

    INCREMENTING VERSION (the most important step)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiomax',
-    version='0.0.2',
+    version='0.0.3',
     description='Max asynchronous API',
     author='oaa dpnspn',
     packages=find_packages(),
