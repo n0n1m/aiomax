e3eec15cf17d2a2cf1978df7a28a8c15355130d7
mbutsk <maratik1036@gmail.com>
2025-04-23 18:40:12 +0300

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiomax',
-    version='0.1.0',
+    version='0.5.0',
     description='Max asynchronous API',
     author='oaa dpnspn',
     packages=find_packages(),
