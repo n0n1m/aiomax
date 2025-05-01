29ecd620a970763db608a1efaa6dbb3b132feb56
mbutsk <maratik1036@gmail.com>
2025-05-01 13:52:15 +0300

    pypi preparation

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup, find_packages
 
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
 setup(
     name='aiomax',
     version='0.6.0',
     description='Max asynchronous API',
     author='oaa dpnspn',
+    author_email='mbutsk@icloud.com',
     packages=find_packages(),
     install_requires=['aiohttp'],
-    zip_safe=False
+    zip_safe=False,
+    url = "https://github.com/dpnspn/aiomax",
+    license="MIT License, see LICENSE.md file",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    project_urls={
+        "GitBook docs": "https://dpnspn.gitbook.io/aiomax"
+    }
 )
