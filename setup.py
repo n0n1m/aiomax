5d01747830d4c691ac651d7f5ef40d96c35817a7
mbutsk <maratik1036@gmail.com>
2025-05-02 18:01:11 +0300

    fix 1 very stupid bug

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='aiomax',
-    version='1.0.1',
+    version='1.0.2',
     description='Max asynchronous API',
     author='oaa dpnspn',
     author_email='mbutsk@icloud.com',
