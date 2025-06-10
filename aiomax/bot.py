ba7b34963038967e0eb15d60f24b917b363394bc
dfkkg <dfkkg@ya.ru>
2025-06-10 23:15:31 +0300

    fix message_id type

--- aiomax/bot.py
+++ aiomax/bot.py
@@ -572,7 +572,7 @@ class Bot(Router):
 
 
     async def edit_message(self,
-        message_id: int,
+        message_id: str,
         text: "str | None" = None,
         format: "Literal['markdown', 'html', 'default'] | None" = 'default',
         reply_to: "int | None" = None,
@@ -618,7 +618,7 @@ class Bot(Router):
 
 
     async def delete_message(self,
-        message_id: int
+        message_id: str
     ):
         '''
         Allows you to delete a message in chat.
@@ -642,7 +642,7 @@ class Bot(Router):
 
 
     # async def get_message(self,
-    #     message_id: int
+    #     message_id: str
     # ) -> Message:
     #     '''
     #     Allows you to fetch message's info.
