ceb35a59d7d42e6968c91694cbe34677adac80d2
mbutsk <mbutsk@icloud.com>
2025-07-07 20:50:40 +0300

    ruff x2

--- aiomax/exceptions.py
+++ aiomax/exceptions.py
@@ -3,10 +3,12 @@ class InvalidToken(Exception):
     Invalid token Exception
     """
 
+
 class AttachmentNotReady(Exception):
     """
     Attachment not ready Exception
     """
+
 
 class ChatNotFound(Exception):
     """
